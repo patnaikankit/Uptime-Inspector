d86160bd4aeab1aee6565ba3b4bc8fbc03c4d0aa
Ankit Patnaik <116827173+patnaikankit@users.noreply.github.com>
2023-08-27 22:53:08 +0530

    main page

--- Client/src/components/Auth/auth.jsx
+++ Client/src/components/Auth/auth.jsx
@@ -1,164 +1,174 @@
 import React, { useState, useEffect } from "react";
 
-import "./auth.css"
+import "./auth.css";
 
 export const Auth = () => {
   // State change
   // to switch between signup and login page
-    const [signupActive, setSignupActive] = useState(false)
-    const [data, setData] = useState({
-        name: "",
-        email: "",
-        password: ""
-    })
-    const [errorMsg, setErrorMsg] = useState("")
-    // to disable the login/signup button once the request has been sent so that it can be proccesed properly
-    const [buttonDisable, setButtonDisable] = useState(false)
-    
-    
-    // Functions
-    // to validate the email format
-    const validateEmail = (email) => {
-        return String(email)
-          .toLowerCase()
-          .match(
-            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-          );
-      };
+  const [signupActive, setSignupActive] = useState(false);
+  const [data, setData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+  const [errorMsg, setErrorMsg] = useState("");
+  // to disable the login/signup button once the request has been sent so that it can be proccesed properly
+  const [buttonDisable, setButtonDisable] = useState(false);
 
-      // signup frontend logic
-    const handleSignup = async () => {
-        if(buttonDisable){
-            return ;
-        }
+  // Functions
+  // to validate the email format
+  const validateEmail = (email) => {
+    return String(email)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      );
+  };
 
-        if (!data || typeof data.name !== 'string' || typeof data.email !== 'string' || typeof data.password !== 'string') {
-          setErrorMsg("All Fields are Required!");
-          return;
-        }
-      
-        if (!data.name.trim() || !data.email.trim() || !data.password) {
-            setErrorMsg("All Fields are Required!");
-            return;
-        }
-      
-
-        if(!validateEmail(data.email)){
-            setErrorMsg("Invalid Email Format!")
-            return ;
-        }
-
-        if(data.password.length < 6){
-            setErrorMsg("Password must of atleast 6 characters")
-            return ;
-        }
-        setErrorMsg("")
-
-        // if all the above checks are passed then disable the button and wait for the response
-        setButtonDisable(true)
-        const response = await fetch("http://localhost:3000/api/v1/user/signup", {
-            method: "POST",
-            headers: {
-                "content-type": "application/json"
-            },
-            body: JSON.stringify({
-                name: data.name.trim(),
-                email: data.email,
-                password: data.password
-            })
-        }).catch((err) => {
-            setErrorMsg("Error while creating the user -> ", err.message)
-        })
-
-        setButtonDisable(false)
-
-        if(!response){
-            setErrorMsg("Error while creating the user!")
-        }
-
-        const result = await response.json()
-
-        if(!result.status){
-            setErrorMsg(result.message)
-            return ;
-        }
-
-        const tokens = result.data.tokens
-
-        localStorage.setItem("tokens", JSON.stringify(tokens));
-
-        window.localStorage.reload()
+  // signup frontend logic
+  const handleSignup = async () => {
+    if (buttonDisable) {
+      return;
     }
 
-
-    const handleLogin = async () => {
-      if(buttonDisable){
-        return ;
+    if (
+      !data ||
+      typeof data.name !== "string" ||
+      typeof data.email !== "string" ||
+      typeof data.password !== "string"
+    ) {
+      setErrorMsg("All Fields are Required!");
+      return;
     }
 
-    if(!data.email.trim() || !data.password){
-        setErrorMsg("All Fields are Required!")
-        return ;
+    if (!data.name.trim() || !data.email.trim() || !data.password) {
+      setErrorMsg("All Fields are Required!");
+      return;
     }
 
-    if(!validateEmail(data.email)){
-        setErrorMsg("Invalid Email Format!")
-        return ;
+    if (!validateEmail(data.email)) {
+      setErrorMsg("Invalid Email Format!");
+      return;
     }
 
-    if(data.password.length < 6){
-        setErrorMsg("Password must of atleast 6 characters")
-        return ;
+    if (data.password.length < 6) {
+      setErrorMsg("Password must of atleast 6 characters");
+      return;
     }
-    setErrorMsg("")
-
+    setErrorMsg("");
 
     // if all the above checks are passed then disable the button and wait for the response
-    setButtonDisable(true)
-    const response = await fetch("http://localhost:3000/api/v1/user/login", {
-        method: "POST",
-        headers: {
-            "content-type": "application/json"
-        },
-        body: JSON.stringify({
-            email: data.email,
-            password: data.password
-        })
+    setButtonDisable(true);
+    const response = await fetch("http://localhost:3000/api/v1/user/signup", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({
+        name: data.name.trim(),
+        email: data.email,
+        password: data.password,
+      }),
     }).catch((err) => {
-        setErrorMsg("Error while login -> ", err.message)
-    })
+      setErrorMsg("Error while creating the user -> ", err.message);
+    });
 
-    setButtonDisable(false)
+    setButtonDisable(false);
 
-    if(!response){
-        setErrorMsg("Error while login!")
+    if (!response) {
+      setErrorMsg("Error while creating the user!");
     }
 
-    const result = await response.json()
+    const result = await response.json();
 
-    if(!result.status){
-        setErrorMsg(result.message)
-        return ;
+    if (!result.status) {
+      setErrorMsg(result.message);
+      return;
     }
 
-    const tokens = result.data.tokens
+    const tokens = result.data.tokens;
 
+    // Store access token in localStorage
     localStorage.setItem("tokens", JSON.stringify(tokens));
 
-    // window.localStorage.reload()
+    // You can also store the token in a state if needed
+    // setAccessToken(tokens.accessToken);
+
+    // Reload the window (you might want to replace this with a more elegant state update)
+    window.location.reload();
+  };
+
+  const handleLogin = async () => {
+    if (buttonDisable) {
+      return;
     }
 
+    if (!data.email.trim() || !data.password) {
+      setErrorMsg("All Fields are Required!");
+      return;
+    }
 
-    useEffect(() => {
-      setData({});
-    }, [signupActive]);
+    if (!validateEmail(data.email)) {
+      setErrorMsg("Invalid Email Format!");
+      return;
+    }
 
+    if (data.password.length < 6) {
+      setErrorMsg("Password must of atleast 6 characters");
+      return;
+    }
+    setErrorMsg("");
 
-    const signup = (
-        <div className="box signup">
-            <p className={"heading"}>SignUp</p>
+    // if all the above checks are passed then disable the button and wait for the response
+    setButtonDisable(true);
+    const response = await fetch("http://localhost:3000/api/v1/user/login", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({
+        email: data.email,
+        password: data.password,
+      }),
+    }).catch((err) => {
+      setErrorMsg("Error while login -> ", err.message);
+    });
 
-        <div className={"val"}>
+    setButtonDisable(false);
+
+    if (!response) {
+      setErrorMsg("Error while login!");
+    }
+
+    const result = await response.json();
+
+    if (!result.status) {
+      setErrorMsg(result.message);
+      return;
+    }
+
+    console.log(response);
+    const tokens = result.data.tokens;
+
+    // Store access token in localStorage
+    localStorage.setItem("tokens", JSON.stringify(tokens));
+
+    // You can also store the token in a state if needed
+    // setAccessToken(tokens.accessToken);
+
+    // Reload the window (you might want to replace this with a more elegant state update)
+    window.location.reload();
+  };
+
+  useEffect(() => {
+    setData({});
+  }, [signupActive]);
+
+  const signup = (
+    <div className="box signup">
+      <p className={"heading"}>SignUp</p>
+
+      <div className={"val"}>
         <label>Name</label>
         <input
           className="input"
@@ -169,7 +179,7 @@ export const Auth = () => {
         />
       </div>
 
-        <div className={"val"}>
+      <div className={"val"}>
         <label>Email</label>
         <input
           className="input"
@@ -180,7 +190,7 @@ export const Auth = () => {
         />
       </div>
 
-        <div className={"val"}>
+      <div className={"val"}>
         <label>Password</label>
         <input
           className="input"
@@ -199,17 +209,17 @@ export const Auth = () => {
       </button>
 
       <p className="bottom-text">
-        Already an user? {" "}
+        Already an user?{" "}
         <span onClick={() => setSignupActive(false)}>Login</span>
       </p>
     </div>
-    )
+  );
 
-    const login = (
-        <div className="box signup">
-            <p className={"heading"}>Login</p>
+  const login = (
+    <div className="box signup">
+      <p className={"heading"}>Login</p>
 
-        <div className={"val"}>
+      <div className={"val"}>
         <label>Email</label>
         <input
           className="input"
@@ -220,7 +230,7 @@ export const Auth = () => {
         />
       </div>
 
-        <div className={"val"}>
+      <div className={"val"}>
         <label>Password</label>
         <input
           className="input"
@@ -239,13 +249,10 @@ export const Auth = () => {
       </button>
 
       <p className="bottom-text">
-        New User? {" "}
-        <span onClick={() => setSignupActive(true)}>SignUp</span>
+        New User? <span onClick={() => setSignupActive(true)}>SignUp</span>
       </p>
     </div>
-    )
+  );
 
-    return (
-        <div className="container">{signupActive ? signup : login}</div>
-    );
-}
+  return <div className="container">{signupActive ? signup : login}</div>;
+};
